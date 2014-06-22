babbfbbe0f6044ba8c541a996a07dc8a99bb8932
Allen Park <allenpark@mit.edu>
2014-06-22 00:19:05 -0700

    first commit

---
+++ js/js.js
@@ -0,0 +1,112 @@
+var TYPE = {
+    EMPTY: " ",
+    PLAYER: "p",
+    WALL: "0",
+    FORWARD: "/",
+    BACKWARD: "\\",
+    BUTTON: ".",
+    SOURCE: "@",
+    GOAL: "!"
+};
+
+var game = {};
+
+game.init = function(map) {
+    this.cellWidth = 30;
+    this.cellHeight = 30;
+    this.lineWidth = 2;
+    this.cellWidth += this.lineWidth;
+    this.cellHeight += this.lineWidth;
+    this.scale = 2;
+    this.cellWidth *= this.scale;
+    this.cellHeight *= this.scale;
+
+    this.canvas = document.getElementById('canvas');
+    this.context = canvas.getContext('2d');
+    this.field = map.field;
+    this.width = this.field.length;
+    this.height = this.field[0].length;
+    this.player = map.player; // {x, y}
+    this.buttons = map.buttons; // [{x1, y1}, ..., {xn, yn}]
+    this.source = map.source; // {x, y}
+    this.goal = map.goal; // {x, y}
+    this.canvas.width = this.width * this.cellWidth + this.lineWidth;
+    this.canvas.height = this.height * this.cellHeight + this.lineWidth;
+
+    window.addEventListener('keydown', this.onKeyPress, false);
+    
+    this.draw();
+};
+
+game.onKeyPress = function(e) {
+    var key = e.keyCode;
+    if (key == 37 || key == 65) { // left
+        console.log("left");
+    } else if (key == 38 || key == 87) { // up
+        console.log("up");
+    } else if (key == 39 || key == 68) { // right
+        console.log("right");
+    } else if (key == 40 || key == 83) { // down
+        console.log("down");
+    }
+    var coords = test_from_allen_code();
+    console.log(coords);
+};
+
+game.draw = function() {
+    this.context.lineWidth = this.lineWidth;
+    this.context.beginPath();
+    for (var i = 0; i <= this.width; i++) { // drawing x gridlines
+        this.context.moveTo(i*this.cellWidth, 0);
+        this.context.lineTo(i*this.cellHeight, this.canvas.height);
+    }
+    for (var j = 0; j <= this.height; j++) { // drawing y gridlines
+        this.context.moveTo(0, j*this.cellWidth);
+        this.context.lineTo(this.canvas.width, j*this.cellHeight);
+    }
+    for (var i = 0; i < this.width; i++) { // drawing field
+        for (var j = 0; j < this.height; j++) {
+            var element = this.field[i][j];
+            var x = this.ftcX(i);
+            var y = this.ftcY(j);
+            if (element === '/') {
+                this.context.moveTo(x + 11*this.scale, y + 22*this.scale);
+                this.context.lineTo(x + 22*this.scale, y + 11*this.scale);
+            } else if (element === '\\') {
+                this.context.moveTo(x + 11*this.scale, y + 11*this.scale);
+                this.context.lineTo(x + 21*this.scale, y + 21*this.scale);
+            } else if (element === '0') {
+                this.context.fillRect(x, y, 32*this.scale, 32*this.scale);
+            }
+        }
+    }
+    this.context.stroke();
+    this.context.beginPath();
+    var playerX = this.ftcX(this.player[0]) + this.cellWidth / 2;
+    var playerY = this.ftcY(this.player[1]) + this.cellHeight / 2;
+    this.context.arc(playerX, playerY, 6*this.scale, 0, Math.PI * 2);
+    this.context.stroke();
+    var xChanges = [this.cellWidth / 2, this.cellWidth, this.cellWidth / 2, 0];
+    var yChanges = [0, this.cellHeight / 2, this.cellHeight, this.cellHeight / 2];
+    var sourceX = this.ftcX(this.source[0]) + xChanges[this.source[2]];
+    var sourceY = this.ftcY(this.source[1]) + yChanges[this.source[2]];
+    for (var r = 0; r < 5*this.scale; r++) {
+        this.context.beginPath();
+        this.context.arc(sourceX, sourceY, r, Math.PI/2 * (this.source[2]), Math.PI/2 * (this.source[2] + 2));
+        this.context.stroke();
+    }
+};
+
+
+
+// field to canvas x
+game.ftcX = function(x) {
+    return x * this.cellWidth;
+};
+
+// field to canvas y
+game.ftcY = function(y) {
+    return y * this.cellHeight;
+};
+
+game.init(map1);
---
+++ js/lasers.js
@@ -0,0 +1,289 @@
+var laser_source;
+var laser_location;
+var laser_dir;
+
+/* Position */
+
+function create_position(x, y) {
+    var pos = Object();
+    pos.x = x;
+    pos.y = y;
+    return pos;
+};
+
+function position_x(pos) {
+    return pos.x;
+};
+
+function position_y(pos) {
+    return pos.y;
+};
+
+function equal_position(pos1, pos2) {
+    return (position_x(pos1) == position_x(pos2) &&
+            position_y(pos1) == position_y(pos2));
+};
+
+/* Vector */
+
+function create_vec(dx, dy) {
+    var vec = Object();
+    vec.dx = dx;
+    vec.dy = dy;
+    return vec;
+};
+
+function vec_dx(vec) {
+    return vec.dx;
+};
+
+function vec_dy(vec) {
+    return vec.dy;
+};
+
+function add_pos_vec(pos, vec) {
+    return create_position(vec_dx(vec),
+                           vec_dy(vec));
+};
+
+
+/* Directions */
+
+var DIR_UP = 0;
+var DIR_RIGHT = 1;
+var DIR_DOWN = 2;
+var DIR_LEFT = 3;
+
+function equal_dir(dir1, dir2) {
+    return dir1 == dir2;
+};
+
+
+function is_vertical_dir(dir) {
+    return dir == DIR_UP || dir == DIR_DOWN;
+};
+
+function is_horizontal_dir(dir) {
+    return dir == DIR_LEFT || dir == DIR_RIGHT;
+};
+
+
+function turn_right(dir) {
+    return (dir + 1) % 4;
+};
+
+function turn_left(dir) {
+    return (dir + 3) % 4;
+};
+
+/* + Coordinate System */
+/* Assumes (0, 0) is in the top-left corner */
+
+var DIR_VECS = [create_vec(0, -1), // UP
+                create_vec(1, 0), // RIGHT
+                create_vec(0, 1), // DOWN
+                create_vec(-1, 0)]; // LEFT
+
+function dir_vec(dir) {
+    return DIR_VECS[dir];
+};
+
+/* -> pos */
+function step_dir(pos, dir) {
+    return add_pos_vec(pos, dir_vec(dir));
+};
+
+/* Laser State */
+
+function create_laser_state(live, position, direction) {
+    var s = Object();
+    s.live = live;
+    s.position = position;
+    s.direction = direction;
+    return s;
+};
+function create_live_laser_state(position, direction) {
+    return create_laser_state(true,
+                              position,
+                              direction);
+};
+function create_dead_laser_state(pos, dir) {
+    return create_laser_state(false,
+                              pos,
+                              dir);
+};
+
+function laser_state_live(laser_state) {
+    return laser_state.live;
+};
+function laser_state_pos(laser_state) {
+    return laser_state.position;
+};
+function laser_state_dir(laser_state) {
+    return laser_state.direction;
+};
+
+/* Reflective Cell Info */
+
+var OPEN = 0;
+var WALL = 1;
+var MIRROR_FORWARD = 2;  //   /
+var MIRROR_BACKWARD = 3; //   \
+
+function get_laser_effect(pos) {
+    var x = position_x(pos);
+    var y = position_y(pos);
+    if (x < 0 || y < 0 || x >= game.width || y >= game.height) {
+        return WALL;
+    }
+    var field_cell = game.field[x][y];
+    
+    if (field_cell == TYPE.FORWARD) {
+        return MIRROR_FORWARD;
+    } else if (field_cell == TYPE.FORWARD) {
+        return MIRROR_FORWARD;
+    } else {
+        return OPEN;
+    }
+};
+
+function absorbs(laser_effect) {
+    return laser_effect == WALL;
+};
+
+function reflects(laser_effect) {
+    return laser_effect == MIRROR_FORWARD ||
+        laser_effect == MIRROR_BACKWARD;
+};
+
+function apply_laser_effect_reflection(dir, laser_effect) {
+    if (laser_effect == MIRROR_FORWARD) {
+        if (is_vertical_dir(dir)) {
+            return turn_right(dir);
+        } else {
+            return turn_left(dir);
+        }
+    } else if (laser_effect == MIRROR_BACKWARD) {
+        if (is_vertical_dir(dir)) {
+            return turn_left(dir);
+        } else {
+            return turn_right(dir);
+        } 
+   }
+    throw "Bad reflection effect";
+};
+
+/* Simulation Stuff */
+
+function create_laser_destination(dest_pos, dest_dir) {
+    // Change to the internal representation of a destination being
+    // entering a cell with a given direction by stepping once in the
+    // given direction.
+
+    return create_live_laser_state(step_dir(dest_pos, dest_dir),
+                                   dest_dir);
+};
+
+function at_destination(laser_state, laser_destination) {
+    return (equal_position(laser_state_pos(laser_state),
+                           laser_state_pos(laser_destination)) &&
+            equual_dir(laser_state_dir(laser_state),
+                       laser_state_pos(laser_destination)));
+};
+
+
+function step_laser_state(init_laser_state) {
+    var init_live = laser_state_live(init_laser_state);
+    if (!init_live) {
+        throw "Stepping dead laser";
+    }
+    var init_pos = laser_state_pos(init_laser_state);
+    var init_dir = laser_state_dir(init_laser_state);
+
+    var new_pos = step_dir(init_pos, init_dir);
+    var laser_effect = get_laser_effect(new_pos);
+
+    var new_dir = init_dir;
+    if (absorbs(laser_effect)) {
+        return create_dead_laser_state(new_pos, new_dir);
+    } else if (reflects(laser_effect)) {
+        var new_dir = apply_reflection(init_dir, refl_state);
+    }
+    return create_live_laser_state(new_pos, new_dir);
+};
+
+function simulate_laser(laser_source, laser_destination) {
+    var laser_coords = [];
+    var laser_state = laser_source;
+    while (laser_state_live(laser_state) &&
+           !at_destination(laser_state, laser_destination)) {
+        laser_coords.push(laser_state_pos(laser_state));
+        var new_laser_state = step_laser_state(laser_state);
+        //render_laser_step(laser_state, new_laser_state);
+        laser_state = new_laser_state;
+    }
+    laser_coords.push(laser_state_pos(laser_state));
+    return laser_coords;
+};
+
+
+function test_from_allen_code() {
+    /* Start with the laser entering the start_pos from the
+     * start_dir */
+    console.log('start test allen');
+    var laser_start_pos = create_position(0, 3);
+    var laser_start_dir = DIR_RIGHT;
+    var laser_source = create_live_laser_state(laser_start_pos,
+                                               laser_start_dir);
+    /* Succeed when the laser is leaving the destination_pos
+     * towards desination_dir */
+    var destination_pos = create_position(1, 1);
+    var destination_dir = DIR_DOWN;
+    var destination = create_laser_destination(destination_pos,
+                                               destination_dir);
+
+    var coords = simulate_laser(laser_source, destination);
+    return coords;
+}
+
+
+/* Sprite-based rendering stuff */
+
+/* Renders the effect of turn to the new_laser_state direction. This
+ * turn happens at the position of new_laser_state */
+
+var DIES_LASER_GRAPHIC = 'l-dies';
+
+function get_laser_graphic_dir_id(dir) {
+    switch (dir) {
+    case DIR_UP:
+        return 'up';
+    case DIR_RIGHT:
+        return 'right';
+    case DIR_DOWN:
+        return 'down';
+    case DIR_LEFT:
+        return 'left';
+    }
+};
+
+function get_laser_change_graphic(init_dir, new_dir, new_live) {
+    if (!new_live) {
+        return DIES_LASER_GRAPHIC;
+    }
+    return 'l-' + get_laser_graphic_dir_id(init_dir) + '_' +
+        get_laser_graphic_dir_id(new_dir);
+};
+
+function render_laser_step(init_laser_state,
+                           new_laser_state) {
+    var graphic_coords = laser_state_pos(new_laser_state);
+
+    var init_dir = laser_state_dir(init_laser_state);
+    var new_live = laser_state_live(new_laser_state);
+    var new_dir = laser_state_dir(new_laser_state);
+
+    var graphic_id = get_laser_change_graphic(init_dir, new_dir, new_live);
+
+    render_sprite(graphic_id, graphic_coords);
+};
