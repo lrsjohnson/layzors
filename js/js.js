c70440f8067aa1665ece74540fb5440f3c3a3bf1
Allen S Park <apark93@mit.edu>
2014-07-01 00:44:37 -0400

    keyboard shortcut for next level

--- js/js.js
+++ js/js.js
@@ -65,6 +65,10 @@ game.handleKeyPress = function(e) {
     if (key == 82) {
         start();
         return;
+    } else if (key == 70) {
+        if (gameWon) {
+            window.location = "../" + nextMap;
+        }
     }
     if (!this.active) {
         return;
@@ -383,7 +387,8 @@ var onFinish = function (won) {
     if (won) {
         console.log('Yay!');
         //text.innerHTML = '<b>Click or press \'r\' to go to the next level.</b>';
-        text.innerHTML = '<a href="../' + nextMap + '">Click to go to the next level (' + nextMap + ')</a>';
+        text.innerHTML = '<a href="../' + nextMap + '">Click or press \'f\' to go to the next level (' + nextMap + ').</a>';
+        gameWon = true;
         //currentMap ++;
         if (currentMap > maps.length) {
             //text.innerHTML = "<b>You're done! Congrats!</b>";
@@ -397,6 +402,7 @@ var onFinish = function (won) {
 
 var start = function() {
     game.init(maps[currentMap-1], onFinish);
+    gameWon = false;
     text.innerHTML = '&nbsp;';
     level.innerHTML = "<h2>Layzors Level: " + currentMap + "</h2>";
 };
@@ -404,5 +410,6 @@ var text = document.getElementById('text');
 var level = document.getElementById('level');
 //var currentMap = 1;
 window.addEventListener('keydown', makeOnKeyPress(game), false);
+var gameWon = false;
 start();
 console.log("What are you doing looking at the console? Your actions have been logged, and we know where you live. Back to the game with you!");
